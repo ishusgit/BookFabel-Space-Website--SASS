92a5ec5ac7cbd3a090c68546d0e2c3ce83ff6743
ishusgit <iswarya.harish25@gmail.com>
2024-09-30 15:21:52 +0700

    Feature: Book website files , folder and coding.

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//back to top btn
+function userScroll(){
+    const navbar = document.querySelector('.main-part');
+    const backtoTop = document.querySelector('#back-to-top');
+    window.addEventListener('scroll', () =>{
+      if(window.scrollY > 50){
+        backtoTop.classList.add('show');
+      }else{
+        backtoTop.classList.remove('show');
+      }
+    });
+    }
+    function scrollToTop() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+    
+    //eventlistener
+    document.addEventListener('DOMContentLoaded', userScroll);
+    document.querySelector('#back-to-top'), addEventListener('click',scrollToTop);
+    //prvent the back to top function from interfering with carousel buttons
+    
